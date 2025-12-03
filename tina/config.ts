6f4dd13fb12156d21b1dbf3b5a5e6fadb68a8792
yun_chen <yun_chen1@happyfan7.com>
2025-12-03 12:09:50 +0800

    Fix TinaCMS branch configuration to use NEXT_PUBLIC_TINA_BRANCH

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tinacms'
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'master'
+const branch = process.env.NEXT_PUBLIC_TINA_BRANCH || process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'master'
 
 export default defineConfig({
     branch,
